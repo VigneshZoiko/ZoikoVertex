f9890db9d316f53d8fd0c46e19366bf53a4a798b
minitgit config --global user.email minit@zoikogroup.comgit config --global user.name minit <minit@zoikogroup.com>
2026-06-13 12:24:15 +0530

    fix: add missing Link import in security page

--- landing/src/app/security/page.tsx
+++ landing/src/app/security/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/Footer";
 import {
